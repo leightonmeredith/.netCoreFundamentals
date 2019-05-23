0c945f3969fcc84209782a8ddd8e0cee52a14d91
Leighton Meredith <leighton.g.meredith@nasa.gov>
2019-05-23 08:32:56 -0400

    build and register data access service and page model

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id=1, Name="Name1", Location="Loca1", Type=CuisineType.Chinese},
+                new Restaurant{Id=1, Name="Name2", Location="Loca2", Type=CuisineType.Chinese},
+                new Restaurant{Id=1, Name="Name21", Location="Loca21", Type=CuisineType.Chinese}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -5,22 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration _config;
+        private readonly IRestaurantData _restaurantData;
 
         public string Msg { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this._config = config;
+            this._restaurantData = restaurantData;
         }
         public void OnGet()
         {
-            Msg = _config["AppSettingMessage"]; //"Hello World";
+            Msg = _config["AppSettingMessage"];
+            Restaurants = _restaurantData.GetAll();
         }
     }
 }
