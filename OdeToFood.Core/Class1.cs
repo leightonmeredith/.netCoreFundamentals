5823501d66acfdba917640daafd3c5417f579de8
Leighton Meredith <leighton.g.meredith@nasa.gov>
2019-05-22 21:33:34 -0400

    inject and use configuration in rasor page

---
+++ OdeToFood.Core/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public CuisineType Type { get; set; }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration _config;
+
+        public string Msg { get; set; }
+
+        public ListModel(IConfiguration config)
+        {
+            this._config = config;
+        }
+        public void OnGet()
+        {
+            Msg = _config["AppSettingMessage"]; //"Hello World";
+        }
+    }
+}
