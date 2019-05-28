a8b39077d06cd887726b8d96dead600d6890b47c
Leighton Meredith <leighton.g.meredith@nasa.gov>
2019-05-28 17:14:37 -0400

    add connection string and tooling

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -8,6 +8,13 @@ namespace OdeToFood.Data
 {
     public class OdeToFoodDbContext: DbContext
     {
+        //options 4wd to DbContext base class; nothing to do with it here
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+            : base(options)
+        {
+            //takes connection options and provider options
+        }
+
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
