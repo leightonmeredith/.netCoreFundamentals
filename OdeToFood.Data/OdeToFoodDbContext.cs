5f132e3f6763bbc78d438d52f824c0d42ae22807
Leighton Meredith <leighton.g.meredith@nasa.gov>
2019-06-04 19:46:36 -0400

    set up auto EF mig

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -5,8 +5,9 @@ namespace OdeToFood.Data
 {
     public class OdeToFoodDbContext : DbContext
     {
-        //dotnet ef migrations add initCreate -s..\OdeToFood\OdeToFood.csproj
-        //dotnet ef dbcontext info -s..\OdeToFood\OdeToFood.csproj
+        //dotnet ef migrations add initCreate -s ..\OdeToFood\OdeToFood.csproj
+        //dotnet ef dbcontext info -s ..\OdeToFood\OdeToFood.csproj
+        //dotnet ef migrations script -s ..\OdeToFood\OdeToFood.csproj
 
         //options 4wd to DbContext base class; nothing to do with it here
         public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
