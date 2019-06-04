6fc7574970a726aa9d7ace3fef0d73f47d1c3407
Leighton Meredith <leighton.g.meredith@nasa.gov>
2019-06-04 19:46:13 -0400

    set up automatic EF migrations

--- OdeToFood/Program.cs
+++ OdeToFood/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
@@ -14,7 +11,20 @@ namespace OdeToFood
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            MigrateDb(host);
+
+            host.Run();
+        }
+
+        private static void MigrateDb(IWebHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<OdeToFoodDbContext>();
+                db.Database.Migrate();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
